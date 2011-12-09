f76ac15710af1e16e236ac1aa6c39945d3c2d15e
alee <zq@alee2002.com>
2011-12-09 16:07:23 +0800

    modify for control panel menu

--- bean/widget/control-panel-frame-menu.conf.php
+++ bean/widget/control-panel-frame-menu.conf.php
@@ -2,17 +2,19 @@
 return array(
 	array(
 		'title' => '开发' ,
+		'link' => '?c=org.opencomb.development.toolkit.platform.ClearCache' ,
 		'menu' => array(
 			'direction' => 'v' ,
-			'tearoff' => true ,
 			'items' => array(
 					array(
 							'title'=>'创建扩展' ,
 							'link' => '?c=org.opencomb.development.toolkit.extension.CreateExtension' ,
+							'quote' => 'c=org.opencomb.development.toolkit.extension.CreateExtension' ,
 					) ,
 					array(
 							'title'=>'清空缓存' ,
 							'link' => '?c=org.opencomb.development.toolkit.platform.ClearCache' ,
+							'quote' => 'c=org.opencomb.development.toolkit.platform.ClearCache' ,
 					) ,
 			) ,
 		) ,
