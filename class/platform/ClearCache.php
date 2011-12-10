9403d7ab34f9359fae474171ebb1cbecc58529a2
alee <zq@alee2002.com>
2011-12-10 21:43:42 +0800

    complete clear cache

--- class/platform/ClearCache.php
+++ class/platform/ClearCache.php
@@ -1,6 +1,8 @@
 <?php
 namespace org\opencomb\development\toolkit\platform ;
 
+use org\jecat\framework\fs\FileSystem;
+
 use org\jecat\framework\setting\Setting;
 
 use org\jecat\framework\message\Message;
@@ -23,16 +25,40 @@ class ClearCache extends ControlPanel
 
 	public function process()
 	{
-		if( $this->params->has('clear_class_signture') )
+		if( $this->params->has('clear_system_cache') )
 		{
-			Setting::singleton()->deleteItem('/platform/class','signture') ;
-			$this->form->createMessage(Message::success,'系统类库　签名的缓存已经被清除') ;
+			if( FileSystem::singleton()->delete('/data/cache/platform/system/objects',true,true) )
+			{
+				$this->form->createMessage(Message::success,'系统缓存 已经被清除') ;				
+			}
+			else
+			{
+				$this->form->createMessage(Message::failed,'系统缓存失败') ;
+			}
 		}
 		
-		if( $this->params->has('clear_template_signture') )
+		if( $this->params->has('clear_class_compiled') )
 		{
-			Setting::singleton()->deleteItem('/platform/template','signture') ;
-			$this->form->createMessage(Message::success,'系统模板库签名的缓存已经被清除') ;
+			if( FileSystem::singleton()->delete('/data/compiled/class',true,true) )
+			{
+				$this->form->createMessage(Message::success,'类编译缓存 已经被清除') ;
+			}
+			else
+			{
+				$this->form->createMessage(Message::failed,'清除类编译缓存失败') ;
+			}	
+		}
+		
+		if( $this->params->has('clear_template_compiled') )
+		{
+			if( FileSystem::singleton()->delete('/data/compiled/template',true,true) )
+			{
+				$this->form->createMessage(Message::success,'模板编译缓存 已经被清除') ;	
+			}
+			else
+			{
+				$this->form->createMessage(Message::failed,'清除模板编译缓存失败') ;
+			}	
 		}
 	}
 
