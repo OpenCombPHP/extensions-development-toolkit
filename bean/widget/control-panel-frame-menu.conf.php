155ff3a7d8e08a30198e582099b7ff4c8e681f4c
alee <zq@alee2002.com>
2011-12-10 19:50:12 +0800

    + AOPManger

--- bean/widget/control-panel-frame-menu.conf.php
+++ bean/widget/control-panel-frame-menu.conf.php
@@ -1,20 +1,39 @@
 <?php
 return array(
-	array(
+	'development' => array(
 		'title' => '开发' ,
 		'link' => '?c=org.opencomb.development.toolkit.platform.ClearCache' ,
 		'menu' => array(
 			'direction' => 'v' ,
 			'items' => array(
-					array(
+					'create-extensions' => array(
 							'title'=>'创建扩展' ,
 							'link' => '?c=org.opencomb.development.toolkit.extension.CreateExtension' ,
 							'quote' => 'c=org.opencomb.development.toolkit.extension.CreateExtension' ,
 					) ,
-					array(
-							'title'=>'清空缓存' ,
+					'workspace' => array(
+							'title'=>'工作台' ,
 							'link' => '?c=org.opencomb.development.toolkit.platform.ClearCache' ,
 							'quote' => 'c=org.opencomb.development.toolkit.platform.ClearCache' ,
+							'menu' => array(
+								'items'=> array(
+									'clear-cache' => array(
+											'title'=>'清空缓存' ,
+											'link' => '?c=org.opencomb.development.toolkit.platform.ClearCache' ,
+											'quote' => 'c=org.opencomb.development.toolkit.platform.ClearCache' ,
+									) ,
+									'aop-manager' => array(
+											'title'=>'AOP管理' ,
+											'link' => '?c=org.opencomb.development.toolkit.compile.AOPManager' ,
+											'quote' => 'c=org.opencomb.development.toolkit.compile.AOPManager' ,
+									) ,
+									'template-weave-manager' => array(
+											'title'=>'模板编织管理' ,
+											'link' => '?c=org.opencomb.development.toolkit.compile.AOPManager' ,
+											'quote' => 'c=org.opencomb.development.toolkit.compile.AOPManager' ,
+									) ,
+								) ,
+							) ,
 					) ,
 			) ,
 		) ,
---
+++ class/compile/AOPManager.php
@@ -0,0 +1,93 @@
+<?php
+namespace org\opencomb\development\toolkit\compile ;
+
+use org\jecat\framework\lang\oop\ClassLoader;
+
+use org\jecat\framework\message\Message;
+
+use org\jecat\framework\lang\aop\Pointcut;
+
+use org\jecat\framework\lang\aop\jointpoint\JointPoint;
+use org\jecat\framework\lang\aop\AOP;
+use org\opencomb\coresystem\mvc\controller\ControlPanel;
+
+class AOPManager extends ControlPanel
+{
+	public function createBeanConfig()
+	{
+		return array(
+			'view:aopManager' => array(
+				'template' => 'AOPManager.html' ,
+			)
+		) ;
+	}
+
+	public function process()
+	{
+		parent::doActions() ;
+		
+		$arrAopDetail = array() ;
+
+		foreach(AOP::singleton()->aspectIterator() as $aAspect)
+		{
+			foreach($aAspect->pointcuts()->iterator() as $aPointcut)
+			{
+				$aPointcut instanceof Pointcut ;
+				
+				foreach($aPointcut->jointPoints()->iterator() as $aJointPoint)
+				{
+					$aJointPoint instanceof JointPoint ;
+					
+					$sClass = $aJointPoint->weaveClass() ;
+					$sDeclare = $aJointPoint->exportDeclare(false) ;
+					
+					if(empty($arrAopDetail[$sClass][$sDeclare]))
+					{
+						$arrAopDetail[$sClass][$sDeclare]['aspects'] = array() ;
+						$arrAopDetail[$sClass][$sDeclare]['advices'] = array() ;
+					}
+					
+					foreach( $aPointcut->advices()->iterator() as $aAdvice )
+					{
+						if( !in_array($aAdvice,$arrAopDetail[$sClass][$sDeclare]['advices'],true) )
+						{
+							$arrAopDetail[$sClass][$sDeclare]['advices'][] = $aAdvice ;
+							$arrAopDetail[$sClass][$sDeclare]['aspects'][] = $aAspect ;
+						}
+					}
+				} 
+			}
+		}
+		
+		$this->aopManager->variables()->set('arrAopDetail',$arrAopDetail) ;
+	}
+	
+	protected function actionClearClassCompliled()
+	{
+		if( empty($this->params['class']) )
+		{
+			$this->aopManager->createMessage(Message::error,'缺少参数') ;
+			return ;
+		}
+		
+		if( !$aCompiledFile = ClassLoader::singleton()->searchClass($this->params['class'],ClassLoader::SEARCH_COMPILED) )
+		{
+			$this->aopManager->createMessage(Message::failed,'没有在系统中找到 class %s 的编译缓存',$this->params['class']) ;
+			return ;
+		}
+		
+		if( $aCompiledFile->delete() )
+		{
+			$this->aopManager->createMessage(Message::success,'class %s 的编译缓存:%s已经删除',array($this->params['class'],$aCompiledFile->url())) ;
+			return ;			
+		}
+		else
+		{
+			$this->aopManager->createMessage(Message::failed,'无法删除class %s 的编译缓存:%s',array($this->params['class'],$aCompiledFile->url())) ;
+			return ;
+		}
+	}
+}
+
+
+?>
