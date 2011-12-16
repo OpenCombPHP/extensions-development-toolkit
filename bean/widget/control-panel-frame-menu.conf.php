97f72d371d1c12ae1888da3b0ec7ecd7bc8b9e83
alee <zq@alee2002.com>
2011-12-16 16:11:00 +0800

    rename "quote" to "query" for menu item bean

--- bean/widget/control-panel-frame-menu.conf.php
+++ bean/widget/control-panel-frame-menu.conf.php
@@ -14,12 +14,12 @@ return array(
 								'create-extensions' => array(
 										'title'=>'创建扩展' ,
 										'link' => '?c=org.opencomb.development.toolkit.extension.CreateExtension' ,
-										'quote' => 'c=org.opencomb.development.toolkit.extension.CreateExtension' ,
+										'query' => 'c=org.opencomb.development.toolkit.extension.CreateExtension' ,
 								) ,
 								'extensionpackage' => array(
 										'title'=>'扩展打包' ,
 										'link' => '?c=org.opencomb.development.toolkit.extension.ExtensionPackages' ,
-										'quote' => 'c=org.opencomb.development.toolkit.extension.ExtensionPackages' ,
+										'query' => 'c=org.opencomb.development.toolkit.extension.ExtensionPackages' ,
 								) ,
 							),
 						),
@@ -27,23 +27,23 @@ return array(
 					'workspace' => array(
 							'title'=>'工作台' ,
 							'link' => '?c=org.opencomb.development.toolkit.platform.ClearCache' ,
-							'quote' => 'c=org.opencomb.development.toolkit.platform.ClearCache' ,
+							'query' => 'c=org.opencomb.development.toolkit.platform.ClearCache' ,
 							'menu' => array(
 								'items'=> array(
 									'clear-cache' => array(
 											'title'=>'清空缓存' ,
 											'link' => '?c=org.opencomb.development.toolkit.platform.ClearCache' ,
-											'quote' => 'c=org.opencomb.development.toolkit.platform.ClearCache' ,
+											'query' => 'c=org.opencomb.development.toolkit.platform.ClearCache' ,
 									) ,
 									'aop-manager' => array(
 											'title'=>'AOP管理' ,
 											'link' => '?c=org.opencomb.development.toolkit.compile.AOPManager' ,
-											'quote' => 'c=org.opencomb.development.toolkit.compile.AOPManager' ,
+											'query' => 'c=org.opencomb.development.toolkit.compile.AOPManager' ,
 									) ,
 									'template-weave-manager' => array(
 											'title'=>'模板编织管理' ,
 											'link' => '?c=org.opencomb.development.toolkit.compile.AOPManager' ,
-											'quote' => 'c=org.opencomb.development.toolkit.compile.AOPManager' ,
+											'query' => 'c=org.opencomb.development.toolkit.compile.AOPManager' ,
 									) ,
 								) ,
 							) ,
