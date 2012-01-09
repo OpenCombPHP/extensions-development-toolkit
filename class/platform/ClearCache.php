6b11dc6b985fcf1311da192838d0a52c2a911513
kongyuan <anubiskong@gmail.com>
2012-01-09 09:50:13 +0800

    修改了h2标题,分离了removecache

--- class/platform/ClearCache.php
+++ class/platform/ClearCache.php
@@ -1,13 +1,8 @@
 <?php
 namespace org\opencomb\development\toolkit\platform ;
 
-use org\jecat\framework\fs\FSIterator;
-
-use org\jecat\framework\lang\oop\ClassLoader;
 use org\jecat\framework\fs\FileSystem;
-use org\jecat\framework\setting\Setting;
 use org\jecat\framework\message\Message;
-use org\jecat\framework\system\Application;
 use org\opencomb\coresystem\mvc\controller\ControlPanel;
 
 class ClearCache extends ControlPanel
@@ -18,7 +13,11 @@ class ClearCache extends ControlPanel
 		return array(
 			'view:form' => array(
 				'template' => 'ClearCache.html' ,
-			)		
+			),
+			'controller:removeCache' => array(
+					'class' => 'org\\opencomb\\development\\toolkit\\platform\\RemoveCache' ,
+			) ,
+			'title' => '清理缓存'
 		) ;
 	}
 
@@ -59,66 +58,5 @@ class ClearCache extends ControlPanel
 				$this->form->createMessage(Message::failed,'清除模板编译缓存失败') ;
 			}
 		}
-		//ajax的清理请求
-		if( $this->params->has('deletePaths') )
-		{
-			$sMessage = '成功清理以下缓存文件 : <br/>';
-			if($dataFolder = FileSystem::singleton()->findFolder('/data/compiled/class/')){
-				foreach($dataFolder->iterator(FSIterator::FOLDER | FSIterator::RETURN_FSO) as $aFolder){
-					foreach($this->params->get('deletePaths') as $sPath){
-						$aFolder->deleteChild( $sPath ,true,true);
-						$sMessage .=  $sPath . "<br/>";
-					}
-				}
-			}
-			exit($sMessage);
-		}
-		$this->viewForm->variables()->set('classJson',json_encode( $this->getTree(ClassLoader::singleton()->packageIterator()) )) ;
-	}
-	
-	private function getTree($aPackageIterator){
-		$arrTree =  array();
-		foreach($aPackageIterator as $package){
-			$ns = $package->ns();
-			$arrNs = explode('\\',$ns);
-			$arrExp = &$arrTree;
-			foreach($arrNs as $ns_cl){
-				$bFound= false;
-				for($i = 0; $i < count($arrExp) ;$i++){
-					if( isset($arrExp[$i]['name']) && $arrExp[$i]['name'] == $ns_cl){
-						$arrExp = &$arrExp[$i]['childs'];
-						$bFound = true;
-						break;
-					}
-				}
-				if(!$bFound){
-					$arrExp[] = array('name'=>$ns_cl , 'childs'=>array());
-					$arrExp = &$arrExp[count($arrExp)-1]['childs'];
-				}
-			}
-			$aFolder = $package->folder();
-			$arrExp = $this->buildNode($aFolder->url(false),$aFolder->path());
-		}
-		return $arrTree;
-	}
-	
-	private function buildNode($sFolderUrl,$aFolderPath){
-		$arrNode = array();
-		$aDirectoryIterator = new \DirectoryIterator($sFolderUrl);
-		foreach($aDirectoryIterator as $fileinfo){
-			if($fileinfo->isDot()){
-				continue;
-			}
-			if($fileinfo->isDir()){
-				$arrNode[]['childs'] = $this->buildNode( $fileinfo->getPathname() , $aFolderPath.'/'.$fileinfo->getFilename() );
-				$arrNode[count($arrNode)-1]['name'] = $fileinfo->getFilename();
-			}else{
-				$arrNode[] = array(
-					'name' => substr($fileinfo->getFilename() , 0 ,strlen($fileinfo->getFilename())-4) ,
-					'filename' => $fileinfo->getFilename()
-				) ;
-			}
-		}
-		return $arrNode;
 	}
 }
