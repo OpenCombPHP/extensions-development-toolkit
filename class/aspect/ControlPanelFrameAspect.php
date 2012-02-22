f5125adc17117a2abd0acdd186b9e2422ac1c4b8
lexdene <elephant_liu@mail.dlut.edu.cn>
2012-02-22 10:51:12 +0800

    for rebuild of menu

--- class/aspect/ControlPanelFrameAspect.php
+++ class/aspect/ControlPanelFrameAspect.php
@@ -30,7 +30,7 @@ class ControlPanelFrameAspect
 
 		// 合并配置数组，增加菜单
 		BeanFactory::mergeConfig(
-				$arrConfig['frameview:frameView']['widget:mainMenu']['items']
+				$arrConfig['frameview:frameView']['widget:mainMenu']
 				, BeanFactory::singleton()->findConfig('widget/control-panel-frame-menu','development-toolkit')
 		) ;
 
