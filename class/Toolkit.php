19e7eea730c09491109c9b953f7376b96f546c79
aleechou <zq@alee2002.com>
2012-06-19 12:23:48 +0800

    control pannel menu

--- class/Toolkit.php
+++ class/Toolkit.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< Updated upstream
 namespace org\opencomb\development\toolkit ;
 
 use org\jecat\framework\bean\BeanFactory;
@@ -7,14 +8,30 @@ use org\jecat\framework\lang\aop\AOP;
 use org\opencomb\platform\ext\Extension;
 use org\opencomb\development\toolkit\aspect\SysteExecuteTimeLog ;
 
+=======
+namespace org\opencomb\development\toolkit ;
+
+use org\opencomb\coresystem\mvc\controller\ControlPanel;
+use org\opencomb\platform\service\Service;
+use org\jecat\framework\bean\BeanFactory;
+use org\jecat\framework\lang\aop\AOP;
+use org\opencomb\platform\ext\Extension;
+
+>>>>>>> Stashed changes
 class Toolkit extends Extension
 {
 	public function load()
 	{
+<<<<<<< Updated upstream
 		$aAop = AOP::singleton() ;
 		$aAop->register('org\\opencomb\\development\\toolkit\\aspect\\ControlPanelFrameAspect') ;
 		
 		if(Platform::singleton()->isDebugging())
+=======
+		ControlPanel::registerMenuHandler( array(__CLASS__,'buildControlPanelMenu') ) ;
+		
+		if(Service::singleton()->isDebugging())
+>>>>>>> Stashed changes
 		{
 			$aAop->register('org\\opencomb\\development\\toolkit\\aspect\\ModelDataUsefulDetecter') ;
 			
